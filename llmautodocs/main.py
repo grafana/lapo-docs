9526038c10273670fd2697e5a9007e1a362702f1
Esteban Beltran <esteban@academo.me>
2025-02-28 11:55:27 +0100

    test with a task agent

--- llmautodocs/main.py
+++ llmautodocs/main.py
@@ -1,12 +1,17 @@
 import langroid.language_models as lm
+import langroid as lr
 from llmautodocs.config.llmconfig import llm_config
 from langroid.language_models import Role, LLMMessage
 
 
 def main():
-    llm = lm.OpenAIGPT(llm_config)
-    messages = [
-        LLMMessage(content="You are a helpful assistant", role=Role.SYSTEM),
-        LLMMessage(content="What is the capital of Ontario?", role=Role.USER),
-    ]
-    response = llm.chat(messages, max_tokens=50)
+    # llm = lm.OpenAIGPT(llm_config)
+    agent = lr.ChatAgent(lr.ChatAgentConfig(llm=llm_config))
+    task = lr.Task(agent, interactive=True)
+
+    task.run()
+# messages = [
+#     LLMMessage(content="You are a helpful assistant", role=Role.SYSTEM),
+#     LLMMessage(content="What is the capital of Ontario?", role=Role.USER),
+# ]
+# response = llm.chat(messages, max_tokens=50)
