f7cd15ec75ff4265f0f41d737dd531fbdec7ca45
Esteban Beltran <esteban@academo.me>
2025-03-03 17:20:03 +0100

    out with the old

--- llmautodocs/__init__.py
+++
@@ -1,1 +0,0 @@
-# Make llmautodocs a proper package
--- llmautodocs/config/llmconfig.py
+++
@@ -1,5 +0,0 @@
-import langroid.language_models as lm
-
-llm_config = lm.OpenAIGPTConfig(
-    chat_model="gemini/" + lm.GeminiModel.GEMINI_2_FLASH
-)
